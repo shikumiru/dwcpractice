e60c7028c76cc68562e99ce033f11a091add91b2
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-25 06:33:01 +0000

    保存

---
+++ ruby/apple.rb
@@ -0,0 +1,9 @@
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このりんごは青森県産でも長野原産でもありません。"
+end
---
+++ ruby/lesson10.rb
@@ -0,0 +1,11 @@
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+class Truck < Car
+end
+
+track = Truck.new
+track.run(5)
